27dc9a5c7bd6f1b266b6fba07e95b735d6f79758
Andrew Liu <aliu1@umd.edu>
2017-04-08 04:23:29 -0400

    Use separate prop types package

--- src/services/Html.jsx
+++ src/services/Html.jsx
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
 
 const Html = ({ state, content }) => {
   const helmet = Helmet.rewind();
