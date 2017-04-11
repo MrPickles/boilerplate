1361ce7f1f4f54e05f8fd33bb8709abab7dec407
Andrew Liu <aliu1@umd.edu>
2017-04-11 14:18:34 -0400

    Add hmr for redux reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,16 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers';
 import middleware from '../middleware';
 
-const configureStore = initialState =>
-  createStore(reducers, initialState, applyMiddleware(...middleware));
+const configureStore = (initialState) => {
+  const store = createStore(reducers, initialState, applyMiddleware(...middleware));
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      // eslint-disable-next-line global-require
+      store.replaceReducer(require().default);
+    });
+  }
+  return store;
+};
 
 export default configureStore;
