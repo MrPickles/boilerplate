f04d6a89b2ad4ae3af8049bd93b1e4d8008ef1c2
Andrew Liu <aliu1@umd.edu>
2017-04-11 14:03:26 -0400

    Add HMR for observables

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,4 +3,12 @@ import rootEpic from '../epics';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
+if (module.hot) {
+  module.hot.accept('../epics', () => {
+    // eslint-disable-next-line global-require
+    const rootEpicHot = require('../epics').default;
+    epicMiddleware.replaceEpic(rootEpicHot);
+  });
+}
+
 module.exports = [epicMiddleware];
