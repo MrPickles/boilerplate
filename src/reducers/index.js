d8862738dcb8459d58f033e4bc1c4b9e91b82f33
Andrew Liu <aliu1@umd.edu>
2017-04-11 22:23:34 -0400

    Fix lint errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,6 +12,6 @@ req.keys().forEach((key) => {
   // Set store name as base filename.
   const storeName = key.replace(/^\..*\/(.*)\.js$/, '$1');
   reducers[storeName] = req(key).default;
-})
+});
 
 export default combineReducers(reducers);
