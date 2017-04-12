f2630b9362c9b2eb45dc8098289cce43d1965c43
Andrew Liu <aliu1@umd.edu>
2017-04-11 23:02:45 -0400

    Add default export location for components

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import IndexPageContainer from './containers/IndexPageContainer';
-import TestPage from './components/TestPage/TestPage';
+import TestPage from './components/TestPage';
 import NotFoundPage from './components/NotFoundPage';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout';
 
 export default (
   <Route path="/" component={Layout}>
