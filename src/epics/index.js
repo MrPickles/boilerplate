6e04a94e1036a9c9a07366e9b49f3598cb7689e6
Andrew Liu <aliu1@umd.edu>
2017-04-11 22:44:30 -0400

    Dynamically fetch all epics and middlewares

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,7 +1,16 @@
 import { combineEpics } from 'redux-observable';
 
-import pingEpic from './pingEpic';
+const epics = [];
 
-const rootEpic = combineEpics(pingEpic);
+// Grab all files (except index and spec files).
+const req = require.context('.', true, /^\.\/((?!index|\.spec).)*\.js$/);
+
+req.keys().forEach((key) => {
+  // Set store name as base filename and append to the epic array.
+  const storeName = key.replace(/^\..*\/(.*)\.js$/, '$1');
+  epics.push(req(key).default);
+});
+
+const rootEpic = combineEpics(...epics);
 
 export default rootEpic;
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,22 @@
 import { createEpicMiddleware } from 'redux-observable';
+import logger from 'redux-logger';
+
 import rootEpic from '../epics';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
+const middleware = [epicMiddleware, logger];
+
+// Grab all files (except index and spec files).
+const req = require.context('.', true, /^\.\/((?!index|\.spec).)*\.js$/);
+
+req.keys().forEach((key) => {
+  // Set store name as base filename and prepend to the middleware array.
+  const storeName = key.replace(/^\..*\/(.*)\.js$/, '$1');
+  middleware.unshift(req(key).default);
+});
+
+// Hot reload epics.
 if (module.hot) {
   module.hot.accept('../epics', () => {
     // eslint-disable-next-line global-require
@@ -11,4 +25,4 @@ if (module.hot) {
   });
 }
 
-module.exports = [epicMiddleware];
+export default middleware;
