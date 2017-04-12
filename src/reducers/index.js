c6507d44e7573e6fdeeed4578ae76afda74a0024
Andrew Liu <aliu1@umd.edu>
2017-04-11 21:32:59 -0400

    Use require.context to dynamic import all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
-import testReducer from './testReducer';
-import pingReducer from './pingReducer';
-
 const reducers = {
   routing,
-  testReducer,
-  pingReducer,
 };
+
+// Grab all files (except index and spec files).
+const req = require.context('.', true, /^\.\/((?!index|\.spec).)*\.js$/);
+
+req.keys().forEach((key) => {
+  // Set store name as base filename.
+  const storeName = key.replace(/^\..*\/(.*)\.js$/, '$1');
+  reducers[storeName] = req(key).default;
+})
 
 export default combineReducers(reducers);
