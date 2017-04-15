aa6a32713ee639d5426573e223ab897bb8df3edd
Andrew Liu <aliu1@umd.edu>
2017-04-14 20:59:04 -0400

    Extract css only for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ const isVendor = ({ userRequest }) => (
   userRequest.match(/\.js$/)
 );
 
+// Common configuration.
 const config = {
   // Include source map in development.
   devtool: debug ? 'inline-source-map' : 'hidden-source-map',
@@ -36,21 +37,10 @@ const config = {
       test: /\.jsx?$/,
       include: /src/,
       loader: 'babel-loader',
-    }, {
-      test: /\.s?css$/,
-      loader: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'resolve-url-loader',
-          'sass-loader?sourceMap',
-        ],
-      }),
     }],
   },
 
   plugins: [
-    new ExtractTextPlugin({ filename: 'public/css/style.css', allChunks: true }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
@@ -70,6 +60,7 @@ const config = {
 };
 
 if (debug) {
+  // Development-specific configuration.
   config.entry.app.unshift(
     'react-hot-loader/patch',
     'webpack-hot-middleware/client'
@@ -79,6 +70,28 @@ if (debug) {
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   );
+  // Load CSS modules normally to allow HMR.
+  config.module.rules.push({
+    test: /\.s?css$/,
+    loader: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'resolve-url-loader', 'sass-loader?sourceMap'],
+  });
+} else {
+  // Production-specific configuration.
+  config.plugins.push(
+    new ExtractTextPlugin({ filename: 'public/css/style.css', allChunks: true })
+  );
+  // Extract styles into real CSS.
+  config.module.rules.push({
+    test: /\.s?css$/,
+    loader: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+        'resolve-url-loader',
+        'sass-loader?sourceMap',
+      ],
+    }),
+  });
 }
 
 module.exports = config;
