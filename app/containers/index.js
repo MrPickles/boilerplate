9f38d0f1aa0b7747d768b5c9b67f46881cae9f6c
Andrew Liu <andrew@lextab.com>
2017-05-26 00:29:10 +0000

    Warn about duplicate imports via logging

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,8 +1,12 @@
 // Grab all js files (except index, test, spec, and storybook files).
 const req = require.context('.', true, /^\.\/((?!index|\.test|\.spec|\.stories).)*\.jsx?$/);
+const logger = require('loglevel');
 
 // Find all js files and export their default modules as its file name.
 req.keys().forEach((key) => {
-  const componentName = key.replace(/^\..*\/([^/]+)\.jsx?$/, '$1');
-  module.exports[componentName] = req(key).default;
+  const containerName = key.replace(/^\..*\/([^/]+)\.jsx?$/, '$1');
+  if (module.exports[containerName]) {
+    logger.error(`The component "${containerName}" has a duplicate import. Please rename them accordingly.`);
+  }
+  module.exports[containerName] = req(key).default;
 });
